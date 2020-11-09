83e64edea3fddf4138e4ef0d4238478feaba1003
RGhoul03 <prozorov1542@yandex.ru>
2020-11-10 00:04:56 +0300

    test1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,36 +8,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent { 
   title = 'my-app'; 
-  firstOperand: number; 
-  secondOperand: number; 
-  operator:string; 
-  operator_2:string;
-  errorMessage: string;
-  result: Boolean; 
-
-  doCalc(){
-    if(this.firstOperand != null && this.secondOperand != null && this.operator != null){
-      if(this.operator != "!"){
-        if(this.operator_2 == "&&"){
-          this.result = Boolean(this.firstOperand && this.secondOperand)
-        }else if(this.operator_2 == "||"){
-          this.result = Boolean(this.firstOperand || this.secondOperand)
-        }
-      }else if(this.operator == "!"){
-        if(this.operator_2 == "&&"){
-          this.result = !Boolean(this.firstOperand && this.secondOperand)
-        }else if(this.operator_2 == "||"){
-          this.result = !Boolean(this.firstOperand || this.secondOperand)
-        }
-      }
-    }else{ 
-      if(this.firstOperand == null){ 
-        this.errorMessage = "Нет первого операнда"; 
-      }else if(this.secondOperand == null){ 
-        this.errorMessage = "Нет второго операнда"; 
-      }else if(this.operator == null){ 
-        this.errorMessage = "Нет оператора"; 
-      } 
-    }  
-  }
 }
